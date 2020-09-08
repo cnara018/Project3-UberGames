b0dd63e6c4c815d8b7f7f3b53fce6a72d13ec41a
Jennifer Veiga <jenniferveiga7@gmail.com>
2020-09-08 19:04:18 -0400

    updated front end

--- client/src/components/trade.js
+++ client/src/components/trade.js
@@ -1,16 +1,26 @@
 import React, { Component } from 'react';
 import { Grid, Cell } from 'react-mdl';
-// import Education from './education';
-// import Experience from './experience';
-// import Skills from './skills';
+import Jumbotron from 'react-bootstrap/Jumbotron'
 
 
 class Trade extends Component {
   render() {
     return(
+      
       <div>
-        login
+
+        <Jumbotron style={{ textAlign: 'center' }}>
+
+          <h1>TRADE LISTINGS</h1>
+          <p>
+            Join the only community of gamers where buyers and sellers have an opportunity
+            to negotiate for a better price on any pre-owned merchandise related to gaming.
+        </p>
+          
+        </Jumbotron>
+
       </div>
+
     )
   }
 }
