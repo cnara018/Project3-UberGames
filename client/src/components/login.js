2983f4f2d5e0b5b0c1241b539ff1f0e95ebcaa5b
isaiah361 <isaiahrule@gmail.com>
2020-09-08 18:48:31 -0400

    login and sign up

--- client/src/components/login.js
+++ client/src/components/login.js
@@ -11,75 +11,31 @@ import Col from 'react-bootstrap/Col'
 class Login extends Component {
   render() {
     return (
-      <div style ={{margin: "auto", width: "50%",  padding: "10px"}}>
-      <Card style ={{margin: "auto", width: "50%",  padding: "10px"}}>
-        <Form>
-          <h3>Sign Up</h3>
-          <Form.Row>
-            <Form.Group as={Col} controlId="formGridEmail">
-              <Form.Label>Email</Form.Label>
-              <Form.Control type="email" placeholder="Enter email" />
+      <div style={{ margin: "auto", width: "50%", padding: "10px" }}>
+        <Card style={{ margin: "auto", width: "50%", padding: "10px" }}>
+          <Form>
+            <h3>Login</h3>
+            <Form.Group controlId="formBasicEmail">
+              <Form.Label>Username</Form.Label>
+              <Form.Control type="username" placeholder="Username" />
+              <Form.Text className="text-muted">
+    </Form.Text>
             </Form.Group>
 
-            <Form.Group as={Col} controlId="formGridPassword">
+            <Form.Group controlId="formBasicPassword">
               <Form.Label>Password</Form.Label>
               <Form.Control type="password" placeholder="Password" />
             </Form.Group>
-          </Form.Row>
-
-          <Form.Row>
-            <Form.Group as={Col} controlId="formGridCity">
-              <Form.Label>City</Form.Label>
-              <Form.Control />
+            <Form.Group controlId="formBasicCheckbox">
+              <Form.Check type="checkbox" label="Check me out" />
             </Form.Group>
-
-            <Form.Group as={Col} controlId="formGridState">
-              <Form.Label>State</Form.Label>
-              <Form.Control />
-            </Form.Group>
-
-            <Form.Group as={Col} controlId="formGridZip">
-              <Form.Label>Zip</Form.Label>
-              <Form.Control />
-            </Form.Group>
-          </Form.Row>
-
-          <Form.Group id="formGridCheckbox">
-            <Form.Check type="checkbox" label="Check me out" />
-          </Form.Group>
-
-          <Button variant="primary" type="submit">
-            Submit
+            <Button variant="primary" type="submit">
+              Submit
           </Button>
-        </Form>
-        </Card>
-      <br></br>
-      
-        <Card style = {{margin: "auto", width: "50%",  padding: "10px"}}>
-        <Form>
-          <h3>Login</h3>
-          <Form.Group controlId="formBasicEmail">
-            <Form.Label>Email address</Form.Label>
-            <Form.Control type="email" placeholder="Enter email" />
-            <Form.Text className="text-muted">
-              We'll never share your email with anyone else.
-    </Form.Text>
-          </Form.Group>
-
-          <Form.Group controlId="formBasicPassword">
-            <Form.Label>Password</Form.Label>
-            <Form.Control type="password" placeholder="Password" />
-          </Form.Group>
-          <Form.Group controlId="formBasicCheckbox">
-            <Form.Check type="checkbox" label="Check me out" />
-          </Form.Group>
-          <Button variant="primary" type="submit">
-            Submit
-          </Button>
-        </Form>
+          </Form>
         </Card>
       </div>
-      
+
     )
   }
 }
