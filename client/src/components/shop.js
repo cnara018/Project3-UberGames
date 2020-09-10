3d6999e0138e83d788dc34a6aa54b648d9040569
Jennifer Veiga <jenniferveiga7@gmail.com>
2020-09-10 13:12:16 -0400

    updating shop

--- client/src/components/shop.js
+++ client/src/components/shop.js
@@ -1,7 +1,4 @@
 import React, { Component } from 'react';
-import Container from 'react-bootstrap/Container'
-import Row from 'react-bootstrap/Row'
-import Col from 'react-bootstrap/Col'
 import Button from 'react-bootstrap/Button'
 import Jumbotron from 'react-bootstrap/Jumbotron'
 import Card from 'react-bootstrap/Card'
