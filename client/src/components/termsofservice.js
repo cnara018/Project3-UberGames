7af3996a8e6cbc739309405539a93fdfc05d4a07
wizeapps <jrgsolis@gmail.com>
2020-09-15 05:53:08 -0400

    Resolve Proxy

--- client/src/components/termsofservice.js
+++ client/src/components/termsofservice.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import { Grid, Cell } from 'react-mdl';
 // import Education from './education';
 // import Experience from './experience';
 // import Skills from './skills';
